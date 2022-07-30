b1f08d0c102acc2b02ac3ddf3209526445c7ddc6
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-30 17:17:14 -0400

    fix test data

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
 
   def index
     @supermarket = Supermarket.find(params[:id])
-    require 'pry'; binding.pry 
   end
 
 end
--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -2,20 +2,20 @@
 
 RSpec.describe 'Item Index Page' do
 it 'shows all items associated with that supermarket and does not show duplicates' do
-    supermarket_1 = Supermarket.create!(id: 10000000000, name: 'Giant', location: "Denver")
+    supermarket_1 = Supermarket.create!(id: 1, name: 'Giant', location: "Denver")
     supermarket_2 = Supermarket.create!(id: 2, name: 'Safeway', location: "Tampa")
     
-    customer_1 = Customer.create!(id: 100000000, name: 'John Doe', supermarket_id: supermarket_1.id)
-    customer_2 = Customer.create!(id: 200000000, name: 'Jane Doe', supermarket_id: supermarket_1.id)
+    customer_1 = Customer.create!(id: 1, name: 'John Doe', supermarket_id: supermarket_1.id)
+    customer_2 = Customer.create!(id: 2, name: 'Jane Doe', supermarket_id: supermarket_1.id)
     
-    item_1 = Item.create!(id: 10000000, name: 'Apple', price: 2)
-    item_2 = Item.create!(id: 2000000, name: 'Pear', price: 3)
-    item_3 = Item.create!(id: 3000000, name: "Bannana", price: 4)
+    item_1 = Item.create!(id: 1, name: 'Apple', price: 2)
+    item_2 = Item.create!(id: 2, name: 'Pear', price: 3)
+    item_3 = Item.create!(id: 3, name: "Bannana", price: 4)
 
-    customer_item_1 = CustomerItem.create!(id: 1000000, customer_id: customer_1.id, item_id: item_1.id)
-    customer_item_2 = CustomerItem.create!(id: 2000000, customer_id: customer_1.id, item_id: item_2.id)
-    customer_item_3 = CustomerItem.create!(id: 3000000, customer_id: customer_1.id, item_id: item_3.id)
-    customer_item_4 = CustomerItem.create!(id: 4000000, customer_id: customer_1.id, item_id: item_3.id)
+    customer_item_1 = CustomerItem.create!(id: 1, customer_id: customer_1.id, item_id: item_1.id)
+    customer_item_2 = CustomerItem.create!(id: 2, customer_id: customer_1.id, item_id: item_2.id)
+    customer_item_3 = CustomerItem.create!(id: 3, customer_id: customer_1.id, item_id: item_3.id)
+    customer_item_4 = CustomerItem.create!(id: 4, customer_id: customer_1.id, item_id: item_3.id)
     
     visit "/supermarkets/#{supermarket_1.id}/items"
 
